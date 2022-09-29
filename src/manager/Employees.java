3fdeaa0dc38c1c9d0d24c5d4932d6b493ee79070
Kshitij0169 <tiwari.ks@northeastern.edu>
2022-09-29 17:57:34 -0400

    Initial Commit

---
+++ src/manager/Employees.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manager;
+
+/**
+ *
+ * @author kshitijkumartiwari
+ */
+public class Employees {
+    
+    private String name;
+    private int eid;
+    private int age;
+    private String gender;
+    private String strtdate;
+    private String lvl;
+    private String teaminf;
+    private String postitle;
+    private int cntctno;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStrtdate() {
+        return strtdate;
+    }
+
+    public void setStrtdate(String strtdate) {
+        this.strtdate = strtdate;
+    }
+
+    public String getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(String lvl) {
+        this.lvl = lvl;
+    }
+
+    public String getTeaminf() {
+        return teaminf;
+    }
+
+    public void setTeaminf(String teaminf) {
+        this.teaminf = teaminf;
+    }
+
+    public String getPostitle() {
+        return postitle;
+    }
+
+    public void setPostitle(String postitle) {
+        this.postitle = postitle;
+    }
+
+    public int getCntctno() {
+        return cntctno;
+    }
+
+    public void setCntctno(int cntctno) {
+        this.cntctno = cntctno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    
+ 
+}
+
